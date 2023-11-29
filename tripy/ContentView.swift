d7748c403e5b5da1d9d4408176a7aa9b25f27fa8
wassinm <contactme.wassim@gmail.com>
2023-11-29 18:20:40 +0100

    login screen

--- tripy/ContentView.swift
+++ tripy/ContentView.swift
@@ -32,68 +32,84 @@ struct Home : View {
         
             ZStack{
                 Image("background").resizable().scaledToFill().edgesIgnoringSafeArea(.all)
-
                 VStack{
                     Image("logo").resizable().frame(width: 60,height: 60)
                     // container login and
-                    VStack{
-                        //Login
+                    ZStack{
+                        //blue container
                         VStack{
                             //Login
-                            //Login text//
-                            HStack{
-                                Text("Login").foregroundColor(Color.white).font(.system(size: 36)).fontWeight(.bold)
-                                Spacer(minLength: 0)
-                            }
-//                          email and password
                             VStack{
-                                //Email Address TextField
+                                //Login
+                                //Login text//
                                 HStack{
-                                    Text("Email Address").foregroundColor(Color.white).font(.system(size: 20)).font(.title).fontWeight(.light)
+                                    Text("Login").foregroundColor(Color.white).font(.system(size: 36)).fontWeight(.bold)
                                     Spacer(minLength: 0)
                                 }
-                                HStack{
-                                    Image(systemName:"envelop").foregroundColor(Color.white)
-                                    TextField("", text: self.$email).foregroundColor(Color.white)
-                                }
-                                Divider().background(Color.white)
-                               
-    
-                                // Password Field
-                                HStack{
-                                    Text("Password").foregroundColor(Color.white).font(.system(size: 20)).font(.title).fontWeight(.light)
-                                    Spacer(minLength: 0)
-                                }
-                                HStack{
-                                    Image(systemName:"envelop.fill").foregroundColor(Color("Color1"))
-                                    SecureField("", text: self.$pass).foregroundColor(Color.white)
-                                }
-                                Divider().background(Color.white)
-
-                                //Forget Password?
-                                HStack{
-                                    Spacer(minLength:0)
-                                    Button(action: {}) {
-                                        Text("Forget Password?")
-                                            .foregroundColor(Color.white.opacity(0.6))
+    //                          email and password and forget
+                                VStack{
+                                    //Email Address TextField
+                                    HStack{
+                                        Text("Email Address").foregroundColor(Color.white).font(.system(size: 20)).font(.title).fontWeight(.light)
+                                        Spacer(minLength: 0)
                                     }
+                                    HStack{
+                                        Image(systemName:"envelop").foregroundColor(Color.white)
+                                        TextField("", text: self.$email).foregroundColor(Color.white)
+                                    }
+                                    Divider().background(Color.white)
+                                   
+        
+                                    // Password Field
+                                    HStack{
+                                        Text("Password").foregroundColor(Color.white).font(.system(size: 20)).font(.title).fontWeight(.light)
+                                        Spacer(minLength: 0)
+                                    }
+                                    HStack{
+                                        Image(systemName:"envelop.fill").foregroundColor(Color("Color1"))
+                                        SecureField("", text: self.$pass).foregroundColor(Color.white)
+                                    }
+                                    Divider().background(Color.white)
+
+                                    //Forget Password?
+                                    HStack{
+                                        Spacer(minLength:0)
+                                        Button(action: {}) {
+                                            Text("Forget Password?")
+                                                .foregroundColor(Color.white.opacity(0.6))
+                                        }
+                                    }
+                                    .padding(.top, 30)
+                                    .padding(.bottom,10)
                                 }
-                                .padding(.top, 30)
-                                .padding(.bottom,10)
+                                .padding(.horizontal)
+                                .padding(.top, 40)
+                                .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.white,lineWidth: 1))
+                                
+
+                                
                             }
                             .padding(.horizontal)
-                            .padding(.top, 40)
-                            .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.white,lineWidth: 1))
-                            
-                        }
-                        .padding(.horizontal)
-                        .padding(.top, 30)
-                        
-    
-                      }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 10)
-                    .fill(Color.blue.opacity(0.7))).frame(width:375)
+                            .padding(.top, 30)
+                          }
+                        .padding(.bottom,50)
+                        .background(RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue.opacity(0.7))).frame(width:375)
+                        HStack{
+                            Button(action: {
+                                // Action to perform when the button is tapped
+                            }) {
+                                Text("Login")
+                                    .padding()
+                                    .font(.title)
+                                    .background(Color.white)
+                                    .foregroundColor(.blue)
+                                    .cornerRadius(10)
+                            }
+                        }.padding(.top,410)
+                    }
+
+                    
                 }
             }
        
